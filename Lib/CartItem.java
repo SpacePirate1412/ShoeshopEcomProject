235cfb1da6b9b42eab27effda5c6cfacc2bfb496
Thanaphat-Gee <thanaphat.ar@ku.th>
2025-09-25 10:05:35 +0700

    Update CartItem.java

---
+++ Lib/CartItem.java
@@ -0,0 +1,39 @@
+/**
+ * Mutable ADT for one cart line (Product + quantity).
+ */
+public class CartItem {
+    private final Product product;
+    private int quantity;
+  
+  
+  private void checkRep() {
+        if (product == null) {
+            throw new RuntimeException("RI violated: product cannot be null.");
+        }
+        if (quantity <= 0) {
+            throw new RuntimeException("RI violated: quantity must be positive.");
+        }
+    }
+/**
+     * Create the list in the shoppingcart
+     * @param product Objectproduct
+     * @param quantity must be positive
+     */
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        checkRep();
+    }
+
+    public Product getProduct() { return product; }
+    public int getQuantity() { return quantity; }
+/**
+     * increase the amount in this list
+     * @param amount the amount that you want to increase(must be positive)
+     */
+    public void increaseQuantity(int delta) {
+        if (delta <= 0) throw new IllegalArgumentException("Increase must be positive.");
+        this.quantity += delta;
+        checkRep();
+    }
+}
