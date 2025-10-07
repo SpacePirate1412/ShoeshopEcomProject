f6c2934618ee2a02c09105a035defcd214aaeb77
Thanaphat-Gee <thanaphat.ar@ku.th>
2025-10-07 13:38:13 +0700

    Delete ShoeStore directory

--- ShoeStore/lib/shipment/Shipment.java
+++
@@ -1,6 +0,0 @@
-package lib.shipment;
-
-
-public class Shipment {
-    
-}
--- ShoeStore/main.java
+++
@@ -1,7 +0,0 @@
-import page.*;
-
-public class main {
-    public static void main(String[] args) {
-        SessionChecker.start();
-    }
-}
